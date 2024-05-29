9fcfb53c2235071020a0c4372627dfea1291e080
Marcus Vinicius <mvb0293@gmail.com>
2024-05-29 15:34:55 -0300

    Adicionando arquivo de teste

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import matplotlib.pyplot as plt
-import tkinter
 from SRC.Dados.DataController import DataControler
 
-locais = ["C:/Users/THIAGO/Desktop/POO/TrabalhoFinal/Projeto-POO/SRC/Dados/Data/DataSets/Bitcoin_26_03_2024-27_05_2024_historical_data_coinmarketcap.csv",
-           "C:/Users/THIAGO/Desktop/POO/TrabalhoFinal/Projeto-POO/SRC/Dados/Data/DataSets/Ethereum_26_03_2024-27_05_2024_historical_data_coinmarketcap.csv",
-           "C:/Users/THIAGO/Desktop/POO/TrabalhoFinal/Projeto-POO/SRC/Dados/Data/DataSets/Solana_29_05_2023-28_05_2024_historical_data_coinmarketcap.csv"]
-
+locais = ["Caminho local do arquivo de dataset do Bitcoin",
+           "Caminho local do arquivo de dataset do Ehtereum",
+           "Caminho local do arquivo de dataset da Solana"]
 DataControler.start(locais)
 df = DataControler.solana_df
 y_pred = DataControler.previsao_solana(5)
