8f887af8abdbc90453ac66b78467f5dbfafd5e61
thiagotvos <thiagotobiasvalente@gmail.com>
2024-05-29 15:30:41 -0300

    Teste

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
+import tkinter
 from SRC.Dados.DataController import DataControler
 
-locais = ["Dados/Data/DataSets/Bitcoin_26_03_2024-27_05_2024_historical_data_coinmarketcap.csv",
-           "Dados/Data/DataSets/Ethereum_26_03_2024-27_05_2024_historical_data_coinmarketcap.csv",
-           "Dados/Data/DataSets/Solana_29_05_2023-28_05_2024_historical_data_coinmarketcap.csv"]
+locais = ["C:/Users/THIAGO/Desktop/POO/TrabalhoFinal/Projeto-POO/SRC/Dados/Data/DataSets/Bitcoin_26_03_2024-27_05_2024_historical_data_coinmarketcap.csv",
+           "C:/Users/THIAGO/Desktop/POO/TrabalhoFinal/Projeto-POO/SRC/Dados/Data/DataSets/Ethereum_26_03_2024-27_05_2024_historical_data_coinmarketcap.csv",
+           "C:/Users/THIAGO/Desktop/POO/TrabalhoFinal/Projeto-POO/SRC/Dados/Data/DataSets/Solana_29_05_2023-28_05_2024_historical_data_coinmarketcap.csv"]
 
 DataControler.start(locais)
 df = DataControler.solana_df
