008fd9f7980647e16419863ef1b7d8a27b22e9f5
Marcus Vinicius <mvb0293@gmail.com>
2024-06-21 13:42:30 -0300

    Pequena atualização

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from SRC.interfaceController import Interface
 
-def main():
-    """
-    Função principal para iniciar a interface de usuário.
+class main:
+    def __init__(self):
+        """
+        Classe para iniciar a interface de usuário.
 
-    Cria uma instância da classe Interface e chama o método inicializar_interface
-    para configurar e exibir a interface gráfica do usuário.
-    """
-    teste = Interface()
-    teste.inicializar_interface()
+        Cria uma instância da classe Interface e chama o método inicializar_interface
+        para configurar e exibir a interface gráfica do usuário.
+        """
+        teste = Interface()
+        teste.inicializar_interface()
 
 if __name__ == "__main__":
-    main()
+    start = main()
     """
-    Executa a função principal se o script for executado diretamente.
+    Instacia a classe main e inica o seu construtor se o script for executado diretamente.
     """
